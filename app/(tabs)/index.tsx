88257a28f71441b459db0d3af3b472e0d8b04483
Halil Furkan <hfurkankarademir@gmail.com>
2024-09-02 00:32:11 +0300

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -31,7 +31,7 @@ export default function HomeScreen() {
                     Check Answer
                 </Text>
             </Pressable>
-            <KeyboardLayout />
+            <KeyboardLayout/>
         </View>
     );
 }
